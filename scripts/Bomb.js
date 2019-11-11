399007435ce4d04d7c25a7518b3ca9cf46e3d42a
Jefferson Meneses <32877861+JeffMeneses@users.noreply.github.com>
2019-11-11 20:25:01 -0300

    Printing the map

---
+++ scripts/Bomb.js
@@ -0,0 +1,8 @@
+class Bomb extends Sprite
+{
+    constructor(posX, posY, width, height, color)
+    {
+        super(posX, posY, width, height, color);
+        this.tempo = 80;
+    }
+}
---
+++ scripts/Character.js
@@ -0,0 +1,8 @@
+class Character extends Sprite
+{
+    constructor(posX, posY, width, height, color)
+    {
+        super(posX, posY, width, height, color);
+        this.speed = 2;
+    }
+}
