d3e8c2a9b75005d54c22332aeaa621622c8ea873
biaoliveira98 <57645715+biaoliveira98@users.noreply.github.com>
2019-11-26 16:42:12 -0300

    Add files via upload

---
+++ Bomberman/scripts/Sprite.js
@@ -0,0 +1,12 @@
+class Sprite
+{
+    constructor(posX, posY, width, height, color)
+    {
+        this.posX = posX;
+        this.posY = posY;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.visible = true;
+    }
+}
